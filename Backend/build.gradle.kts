0174217ad764e061f385b1cb24805bab5d11ae04
HheZi <paveltseva@gmail.com>
2024-10-11 00:46:00 +0300

    Api gateway added to project

---
+++ Backend/build.gradle.kts
@@ -0,0 +1,11 @@
+plugins {
+    id("org.springframework.boot") version "3.3.4"
+    id("io.spring.dependency-management") version "1.1.6"
+    java
+}
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(18))
+    }
+}
