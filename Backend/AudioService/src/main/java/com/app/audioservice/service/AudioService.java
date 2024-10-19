d825b6b0b6f53b760bca40d28c4bab4e3894a66d
HheZi <paveltseva@gmail.com>
2024-10-20 01:00:37 +0300

    The audio service is finally working properly

--- Backend/AudioService/src/main/java/com/app/audioservice/service/AudioService.java
+++ Backend/AudioService/src/main/java/com/app/audioservice/service/AudioService.java
@@ -5,6 +5,7 @@ import static java.nio.file.StandardOpenOption.CREATE_NEW;
 import static java.nio.file.StandardOpenOption.WRITE;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URI;
@@ -27,54 +28,41 @@ import com.app.audioservice.utils.AudioFragment;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 
-
 @Service
 public class AudioService {
 
 	public final int MAX_CHUNK_OF_AUDIO = 1024 * 1024;
-	
-	private final String AUDIO_FILE;
-	
-	private final String AUDIO_PATH;
-	
-	private String audioType;
-	
-	@Autowired
-	private ResourceLoader resourceLoader;
 
-	public AudioService(@Value("${audio.path}") String path, @Value("${audio.type}") String type) {
-		this.AUDIO_PATH = !path.endsWith("/") ? path + "/" : path;
-		this.AUDIO_FILE = AUDIO_PATH + "%s." + type;
-		this.audioType = type;
-	}
-	
+	@Value("${audio.path}")
+	private String audioPath;
+
+
 	public AudioFragment getResource(String filename, String rangeHeader) throws IOException {
-		Resource resource = resourceLoader.getResource(AUDIO_FILE.formatted(filename));
+		File file = new File(audioPath, filename);
 
-		List<HttpRange> ranges = HttpRange.parseRanges(rangeHeader);
-		long contentLength = resource.contentLength();
+		try (InputStream input = new FileInputStream(file)) {
+			List<HttpRange> ranges = HttpRange.parseRanges(rangeHeader);
+			long contentLength = file.length();
 
-		HttpRange httpRange = ranges.get(0);
+			HttpRange httpRange = ranges.get(0);
 
-		long rangeStart =  httpRange.getRangeStart(contentLength);
-		long rangeEnd =  rangeStart + MAX_CHUNK_OF_AUDIO;
-		
-		try (InputStream input = resource.getInputStream()) {
+			long rangeStart = httpRange.getRangeStart(contentLength);
+			long rangeEnd = rangeStart + MAX_CHUNK_OF_AUDIO;
+
 			input.skipNBytes(rangeStart);
-			return new AudioFragment(input.readNBytes(MAX_CHUNK_OF_AUDIO), createRangeHeaderValue(rangeStart, rangeEnd, contentLength));
+			return new AudioFragment(input.readNBytes(MAX_CHUNK_OF_AUDIO),
+					createRangeHeaderValue(rangeStart, rangeEnd, contentLength));
+
 		}
 
 	}
 
 	@SneakyThrows
 	public void saveAudio(SaveAudioDTO dto) {
-		
-		
-		Path path = Path.of(resourceLoader.getResource(AUDIO_PATH).getURI()).resolve(dto.getName() + audioType);
-		
+		Path path = Path.of(audioPath).resolve(dto.getName());
 		Files.write(path, dto.getContent());
 	}
-	
+
 	private String createRangeHeaderValue(long startRange, long endRange, long contentLength) {
 		return String.format("bytes %d-%d/%d", startRange, endRange, contentLength);
 	}
